d18b2ac184e681e403804d59ba10532383d674e7
ColeHan_o <hanke2005@gmail.com>
2019-02-18 13:17:46 +0800

    update for second class

---
+++ imooc_Linux_c_2nd/pointer/main2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void change(int a, int b)
+{
+  int tmp=a;
+  a=b;
+  b=tmp;
+}
+
+int main()
+{
+    int a=5;
+    int b=3;
+    change(a,b);//&get the address;
+
+    printf("a=%d\nb=%d\n",a,b);
+
+    return 0;
+}
